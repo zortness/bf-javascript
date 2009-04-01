ed43e9cf464b172bf9903b64afb0027bfbadad5d
zortness <zortness@69122a38-c712-11dd-8772-d1b041bf0e37>
2009-04-01 20:22:17 +0000

    Cleaned up the test cases and added a combined test.

---
+++ trunk/tests/bfhwCompilerAlwaysEvalTest.js
@@ -0,0 +1,26 @@
+/* hello world speed test for compiled bf code */
+document.write ('This program compiles the BF "Hello World!" program into JavaScript and runs it numerous times to get an average execution time.<br /><br />');
+bfc = new BFJSCompiler ();
+var cycles = 1000;
+var helloWorld = '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.';
+document.write ('Compiling program with <b>alwayseval</b> mode...');
+var startCompileTime = new Date ();
+var prog = bfc.compile (helloWorld, 'alwayseval');
+document.write ('Done.<br />');
+document.write ('Executing program ' + cycles + ' times...');
+var startTime = new Date ();
+for (var i = 0; i < cycles; i++)
+{
+	prog.run ();
+}
+var stopTime = new Date ();
+var compileTime = startTime - startCompileTime;
+var execTime = stopTime - startTime;
+var avg = execTime / cycles;
+document.write ('Done.<br />');
+document.write ('Compile Time: ' + compileTime + ' ms<br />');
+document.write ('Start Time: ' + startTime + '<br />');
+document.write ('Stop Time: ' + stopTime + '<br />');
+document.write ('Total Execution Time: ' + execTime + ' ms<br />');
+document.write ('Average Execution Time: ' + avg + ' ms<br /><br />');
+document.write ('Program Output: <b>' + prog.run () + '</b><br />');
--- trunk/tests/fullTest.js
+++ trunk/tests/fullTest.js
@@ -1,30 +1,56 @@
-/* hello world speed test for compiled bf code */
-document.write ('This program compiles the BF "Hello World!" program into JavaScript and runs it numerous times to get an average execution time.<br /><br />');
-bfc = new BFJSCompiler ();
+/**
+ * Interpreter Test
+ */
+document.write ('This program will run the BF "Hello World!" script through the Interpreter and Compiler numerous times to get an average execution time.<br /><br />');
 var cycles = 1000;
 var helloWorld = '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.';
-document.write ('Compiling program with <b>alwayseval</b> mode...');
-var startCompileTime = new Date ();
-var prog = bfc.compile (helloWorld, 'alwayseval');
-document.write ('Done.<br />');
-document.write ('Executing program ' + cycles + ' times...');
+bfi = new BFJSInterpreter ();
+document.write ('Executing interpreter ' + cycles + ' times...');
 var startTime = new Date ();
 for (var i = 0; i < cycles; i++)
 {
-	prog.run ();
+	bfi.execute (helloWorld);
+	bfi.reset ();
 }
 var stopTime = new Date ();
-var compileTime = startTime - startCompileTime;
 var execTime = stopTime - startTime;
 var avg = execTime / cycles;
+document.write ('Done.<br />');
+document.write ('Start Time: ' + startTime + '<br />');
+document.write ('Stop Time: ' + stopTime + '<br />');
+document.write ('Total Execution Time: ' + execTime + ' ms<br />');
+document.write ('Average Execution Time: ' + avg + ' ms<br />');
+bfi.execute ('++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.');
+document.write ('Output from interpreter: <b>' + bfi.outStream + '</b><br/><br/>');
+
+/**
+ * Compiler Tests
+ */
+// alwayseval mode
+bfc = new BFJSCompiler ();
+document.write ('Compiling program with <b>alwayseval</b> mode...');
+startCompileTime = new Date ();
+var prog1 = bfc.compile (helloWorld, 'alwayseval');
+document.write ('Done.<br />');
+document.write ('Executing program ' + cycles + ' times...');
+startTime = new Date ();
+for (var i = 0; i < cycles; i++)
+{
+	prog1.run ();
+}
+stopTime = new Date ();
+compileTime = startTime - startCompileTime;
+execTime = stopTime - startTime;
+avg = execTime / cycles;
 document.write ('Done.<br />');
 document.write ('Compile Time: ' + compileTime + ' ms<br />');
 document.write ('Start Time: ' + startTime + '<br />');
 document.write ('Stop Time: ' + stopTime + '<br />');
 document.write ('Total Execution Time: ' + execTime + ' ms<br />');
-document.write ('Average Execution Time: ' + avg + ' ms<br /><br />');
-document.write ('Program Output: <b>' + prog.run () + '</b><br />');
+document.write ('Average Execution Time: ' + avg + ' ms<br />');
+document.write ('Program Output: <b>' + prog1.run () + '</b><br /><br />');
 
+// preeval mode
 document.write ('<br/>Compiling program with <b>preeval</b> mode...');
 startCompileTime = new Date ();
 var prog2 = bfc.compile (helloWorld, 'preeval');
@@ -44,9 +70,10 @@ document.write ('Compile Time: ' + compileTime + ' ms<br />');
 document.write ('Start Time: ' + startTime + '<br />');
 document.write ('Stop Time: ' + stopTime + '<br />');
 document.write ('Total Execution Time: ' + execTime + ' ms<br />');
-document.write ('Average Execution Time: ' + avg + ' ms<br /><br />');
-document.write ('Program Output: <b>' + prog2.run () + '</b><br />');
+document.write ('Average Execution Time: ' + avg + ' ms<br />');
+document.write ('Program Output: <b>' + prog2.run () + '</b><br /><br />');
 
+// dump mode
 document.write ('<br/>Compiling program with <b>dump</b> mode...');
 startCompileTime = new Date ();
 var prog3 = bfc.compile (helloWorld, 'dump');
@@ -66,5 +93,5 @@ document.write ('Compile Time: ' + compileTime + ' ms<br />');
 document.write ('Start Time: ' + startTime + '<br />');
 document.write ('Stop Time: ' + stopTime + '<br />');
 document.write ('Total Execution Time: ' + execTime + ' ms<br />');
-document.write ('Average Execution Time: ' + avg + ' ms<br /><br />');
-document.write ('Program Output: <b>' + prog3.run () + '</b><br />');
+document.write ('Average Execution Time: ' + avg + ' ms<br />');
+document.write ('Program Output: <b>' + prog3.run () + '</b><br /><br />');
